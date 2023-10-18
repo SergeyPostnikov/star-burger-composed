a2c19e003dfe2a331bacf4088d2a9f4d584ef63f
Sergey Postnikov <ser.post2017@gmail.com>
2023-10-18 18:32:32 +0300

    set DEBUG  safe default

--- star_burger/settings.py
+++ star_burger/settings.py
@@ -13,7 +13,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 
 SECRET_KEY = env('SECRET_KEY')
-DEBUG = env.bool('DEBUG', True)
+DEBUG = env.bool('DEBUG', False)
 
 GEOCODER_KEY = env('GEOCODER_KEY')
 
