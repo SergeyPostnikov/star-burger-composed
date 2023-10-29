d9c4f024bc3e893c4f7f365bdb4e7ed86e8de86e
Sergey Postnikov <ser.post2017@gmail.com>
2023-10-29 18:58:41 +0300

    update deploy script

--- deploy.sh
+++ deploy.sh
@@ -2,26 +2,33 @@
 
 set -e
 
-echo 'load .env'
+# Цвет ANSI для фиолетового текста
+PURPLE='\e[35m'
+# Сброс цвета (вернуть к стандартному)
+RESET='\e[0m'
+
+echo -e "${PURPLE}Load .env${RESET}"
 source .env
 
-echo 'start git pull'
+echo -e "${PURPLE}Pulling changes${RESET}"
 git pull
-echo 'migrate'
+
+echo -e "${PURPLE}Apply migrations${RESET}"
 /opt/star-burger/env/bin/python manage.py migrate
-echo 'collectstatic'
-/opt/star-burger/env/bin/python manage.py collectstatic
-echo 'installing js packages'
+
+echo -e "${PURPLE}Collect static${RESET}"
+/opt/star-burger/env/bin/python manage.py collectstatic --noinput --clear
+
+echo -e "${PURPLE}Installing js packages${RESET}"
 npm ci --dev
 
-echo 'parcel build bundles'
-
+echo -e "${PURPLE}Parcel build bundles${RESET}"
 ./node_modules/.bin/parcel watch bundles-src/index.js --dist-dir bundles --public-url="./" &
 
-echo 'reload star-burger daemon'
+echo -e "${PURPLE}Reload star-burger daemon${RESET}"
 systemctl restart star-burger
 
-echo 'send deploy info to rollbar'
+echo -e "${PURPLE}Send deploy info to rollbar${RESET}"
 
 COMMIT_HASH=$(git rev-parse HEAD)
 curl -H "X-Rollbar-Access-Token: $ROLLBAR_KEY" \
@@ -29,5 +36,5 @@ curl -H "X-Rollbar-Access-Token: $ROLLBAR_KEY" \
      -X POST 'https://api.rollbar.com/api/1/deploy' \
      -d "{\"environment\": \"production\", \"revision\": \"$COMMIT_HASH\", \"status\": \"succeeded\"}"
 
-echo 'deploy completed'
+echo -e "\n${PURPLE}deploy completed${RESET}"
 
