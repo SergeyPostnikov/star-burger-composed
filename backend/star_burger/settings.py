ca11507911b042f6f72a5cf400d6ee79a4b99005
Sergey Postnikov <ser.post2017@gmail.com>
2023-12-12 19:04:43 +0300

    add default geokcoder key

--- backend/star_burger/settings.py
+++ backend/star_burger/settings.py
@@ -16,7 +16,7 @@ ENVIRONMENT = env.str('ENVIRONMENT', 'development')
 SECRET_KEY = env('SECRET_KEY')
 DEBUG = env.bool('DEBUG', False)
 
-GEOCODER_KEY = env('GEOCODER_KEY')
+GEOCODER_KEY = env('GEOCODER_KEY', None)
     
 ALLOWED_HOSTS = env.list('ALLOWED_HOSTS', ['127.0.0.1', 'localhost'])
 
